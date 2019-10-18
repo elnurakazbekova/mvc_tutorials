5eaa2e5654ba7920211e9914774aa2951e491474
Elnura Kazbekova <elnurakazbekova@gmail.com>
2019-10-18 15:19:49 +0600

    lab4 movies

--- project/Models/Movies/Movie.cs
+++ project/Models/Movies/Movie.cs
@@ -1,38 +1,46 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace project
+
+namespace project.Models.Movies
 {
-    [Display(Name = "Id")]
-    public int Id { get; set; }
+    public class Movie
+    {
+        /// <summary>
+        /// Movie id
+        /// </summary>
+        [Display(Name = "Id")]
+        public int Id { get; set; }
 
-    /// <summary>
-    /// Movie poster 
-    /// </summary>
-    [Display(Name = "Url to poster")]
-    public string Poster { get; set; }
+        /// <summary>
+        /// Movie poster 
+        /// </summary>
+        [Display(Name = "Url to poster")]
+        public string Poster { get; set; }
 
-    /// <summary>
-    /// Movie name
-    /// </summary>
-    [Display(Name = "Movie name")]
-    public string Name { get; set; }
+        /// <summary>
+        /// Movie name
+        /// </summary>
+        [Display(Name = "Movie name")]
+        public string Name { get; set; }
 
-    /// <summary>
-    /// Author
-    /// </summary>
-    [Display(Name = "Author name")]
-    public string Author { get; set; }
+        /// <summary>
+        /// Author
+        /// </summary>
+        [Display(Name = "Author name")]
+        public string Author { get; set; }
 
-    /// <summary>
-    /// Movie genere
-    /// </summary>
-    [Display(Name = "List of genres (by comma)")]
-    public string Genre { get; set; }
+        /// <summary>
+        /// Movie genere
+        /// </summary>
+        [Display(Name = "List of genres (by comma)")]
+        public string Genre { get; set; }
 
-    /// <summary>
-    /// Creation date
-    /// </summary>
-    [Display(Name = "Release date")]
-    public DateTime CreatedAt { get; set; }
+        /// <summary>
+        /// Creation date
+        /// </summary>
+        [Display(Name = "Release date")]
+        public DateTime CreatedAt { get; set; }
+    }
+
 }
